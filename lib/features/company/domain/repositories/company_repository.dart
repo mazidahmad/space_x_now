c9717de4d11bc3eec7e6912e9c683926777621fb
Mazid Ahmad <maazidahmad@gmail.com>
2025-09-10 07:34:34 +0700

    chore(features): Removes outof scopes features

--- lib/features/company/domain/repositories/company_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../../../../core/utils/result.dart';
-import '../entities/company.dart';
-
-abstract class CompanyRepository {
-  Future<Result<Company>> getCompanyInfo();
-}
--- lib/features/crew/domain/repositories/crew_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../../../../core/utils/result.dart';
-import '../entities/crew_member.dart';
-
-abstract class CrewRepository {
-  Future<Result<List<CrewMember>>> getAllCrew();
-  Future<Result<CrewMember>> getCrewById(String id);
-  Future<Result<List<CrewMember>>> queryCrew(Map<String, dynamic> query);
-}
